7e39bdac2f850744c7f39343d833b6172ff7c0a2
Kevin Dean <kevinedean@gmail.com>
2015-01-06 22:17:54 -0600

    project update

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject cfft "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "A pure clojure implementation of the forward and inverse discrete fourier transforms"
+  :url "https://github.com/kedean/cfft"
+  :license {:name "MIT License"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :main ^:skip-aot cfft.core
   :target-path "target/%s"
